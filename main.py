9715aabb4dbbbffd2e2f554cc69e7d8ff2d4bbfc
Alex Gear <alexgear@gmail.com>
2023-01-26 14:55:23 -0700

    Added timer

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from controller import Controller
+import time
 
 def main():
-    # TODO Add git ignore
     # TODO Figure out why drum tracks aren't named
     # TODO remove print statements
     # TODO Encapsulate track???
     # TODO first track should be 01
     # TODO Figure out reverb
-    # TODO Add time calculator
     # TODO Add multithreading
     # TODO Don't bounce empty tracks (drums)
     # TODO add test suite
     # TODO add type hints
+
+    # Grab Currrent Time Before Running the Code
+    start = time.time()
 
     # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aphids/Aphids2.mid"
     # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/0.7/0.71.mid"
@@ -22,5 +24,13 @@ def main():
     controller = Controller(midi_file_path)
     controller.extract_midi_stems()
     controller.convert_to_wav(path=controller.midi_stem_path)
+
+    # Grab Currrent Time After Running the Code
+    end = time.time()
+
+    #Subtract Start Time from The End Time
+    # total_time = end - start
+    # print("\n"+ str(total_time))
+    print(f"Time taken: {end - start}")
 
 main()
