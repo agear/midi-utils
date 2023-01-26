faf6c2a29249bb986970a4eb235b32119d55bcc9
Alex Gear <alexgear@gmail.com>
2023-01-25 22:52:13 -0700

    Added new song for testing

--- main.py
+++ main.py
@@ -78,10 +78,12 @@ def main():
 
     # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aphids/Aphids2.mid"
     # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/0.7/0.71.mid"
-    midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aleksei's Visions of Chess War/Aleksei's Visions of Chess War.mid"
+    # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aleksei's Visions of Chess War/Aleksei's Visions of Chess War.mid"
+    midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/The Human Body (Can Run On Sugar Alone Goddamnit!)/The human body can run on sugar alone, God damn it!8.mid"
+
 
     controller = Controller(midi_file_path)
     controller.extract_midi_stems()
-    # controller.convert_to_wav(path=controller.midi_stem_path)
+    controller.convert_to_wav(path=controller.midi_stem_path)
 
 main()
