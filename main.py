71a270c2c6e2bf1cbfb28173e0bdc5b0a0544316
Alex Gear <alexgear@gmail.com>
2023-01-25 23:07:01 -0700

    More TODOs

--- main.py
+++ main.py
@@ -80,6 +80,9 @@ def main():
     # TODO remove print statements
     # TODO Encapsulate track???
     # TODO first track should be 01
+    # TODO Figure out reverb
+    # TODO Add time calculator
+    # TODO Add multithreading
 
     # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aphids/Aphids2.mid"
     # midi_file_path="/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/0.7/0.71.mid"
