32a2588503ccd6061ce7321f97bb85ed07b7b6b6
Alex Gear <alexgear@gmail.com>
2023-11-18 22:01:03 -0500

    extract midi drum stems

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from typing import List
 
-convert_to_wav: bool = False
-midi_file_paths: List[str] = ["/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aphids/Aphids2.mid",
+convert_to_wav: bool = True
+midi_file_paths: List[str] = [
+                              "/Users/agear/Documents/Projects/AGM/percussion_test.mid",
+                              "/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aphids/Aphids2.mid",
                               "/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/0.7/0.71.mid",
-                              # "/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aleksei's Visions of Chess War/Aleksei's Visions of Chess War.mid",
+                              "/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/Aleksei's Visions of Chess War/Aleksei's Visions of Chess War.mid",
                               "/Users/agear/Documents/Projects/AGM/Guitar Pro PDFs/Finished/The Human Body (Can Run On Sugar Alone Goddamnit!)/The human body can run on sugar alone, God damn it!8.mid",
                               ]
--- midi_track_ag.py
+++ midi_track_ag.py
@@ -1,12 +1,14 @@
+import os
 from midi_event import Midi_Event
 from typing import List
 from copy import deepcopy
 import midi
+from percussion_instrument import Percussion_Instrument
 
 class Midi_Track_AG:
     def __init__(self, events: List[Midi_Event], track_number: int, controller):
         self.events: List[Midi_Event] = events
-        self.programs: List[str] = self._get_programs()
+        self.programs: List[str] = self._get_program_names()
         self.transport_track = controller.transport_track
         self.resolution: int = controller.resolution
         self.midi_stem_path: str = controller.midi_stem_path
@@ -14,6 +16,7 @@ class Midi_Track_AG:
         self.track_number: str = self._get_formatted_track_number(i=track_number)
         self.patterns: List[midi.Pattern] = []
         self.drums: str = self._is_drum_track()
+        self.is_drums: bool = False
         self.extract_programs()
 
     def _is_drum_track(self) -> str:
@@ -23,6 +26,7 @@ class Midi_Track_AG:
                 print(event.event)
                 if event.event.channel == 9:
                     print("FOUND A DRUM TRACK!!!!")
+                    self.extract_midi_drum_stems(i=self.track_number, track=self.events)
                     return "- 0 - Drum Kit 0 "
             # except:
             #     if event.event.channel == 9:
@@ -30,14 +34,14 @@ class Midi_Track_AG:
 
         return ""
 
-    def _get_programs(self):
-        programs = set()
+    def _get_program_names(self):
+        program_names = set()
         for event in self.events:
-            programs.add(event.program_name)
+            program_names.add(event.program_name)
 
 
-        print(f'Programs: {list(programs)}')
-        return list(programs)
+        print(f'Programs: {list(program_names)}')
+        return list(program_names)
 
     def _get_formatted_track_number(self, i):
         if len(str(i+1)) == 2:
@@ -48,11 +52,14 @@ class Midi_Track_AG:
 
     def extract_programs(self):
         print("Extracting programs...")
+        if self.is_drums:
+            self.controller.extract_midi_drum_stems(i=self.track_number, track=self.events)
 
         for program_name in self.programs:
             if program_name == 'None':
                 print("Program is none")
                 continue
+
 
             pattern = midi.Pattern(resolution=self.resolution)
             pattern.append(self.transport_track)
@@ -63,7 +70,7 @@ class Midi_Track_AG:
                     track.append(deepcopy(event.event))
                 elif type(event.event) == midi.NoteOnEvent and event.program_name != program_name:
                     event_copy = deepcopy(event.event)
-                    event_copy.data[1] = 0 # make any note ons that aren't in the current program name have 0 velocity
+                    event_copy.data[1] = 0 # make any NoteOns that aren't in the current program name have 0 velocity
                     # print(f"Appending {event_copy}")
                     track.append(event_copy)
                 else:
@@ -71,9 +78,9 @@ class Midi_Track_AG:
                     track.append(deepcopy(event.event))
             pattern.append(track)
 
-            if program_name == "0 - Drum Kit 0 ":
-                # TODO: get specific percussion instrument name
-                assert False
+            # if program_name == "0 - Drum Kit 0 ":
+            #     # TODO: get specific percussion instrument name
+            #     assert False
             self.patterns.append((program_name, pattern))
 
         # print(self.patterns)
@@ -86,3 +93,60 @@ class Midi_Track_AG:
             filename: str = f"{self.midi_stem_path}/{self.songname} - {self.track_number} {self.drums}- {instrument_name}.mid"
             midi.write_midifile(filename, pattern[1])
 
+    def extract_midi_drum_stems(self, i: int, track: midi.Track) -> None:
+        """
+            Extract MIDI drum stems from the multitrack and save them as separate MIDI files.
+
+            Args:
+                i (int): Track number.
+                track (midi.Track): MIDI drum track.
+        """
+        percussion_instruments: List[Percussion_Instrument] = self.get_percussion_instruments(track)
+
+        percussion_path: str = f"{self.midi_stem_path}/{self.songname} - {self.track_number} - 0 - Drum Kit 0"
+        print(percussion_path)
+
+        try:
+            os.mkdir(percussion_path)
+        except:
+            print("Can't make percussion path")
+            quit()
+
+        for instrument in percussion_instruments:
+            # TODO go through track and mute all other instruments...
+            # TODO figure out of track is empty and delete it
+            print(f"Looking for {instrument}")
+            pattern: midi.Pattern = midi.Pattern(resolution=self.resolution)
+            if self.transport_track:
+                pattern.append(self.transport_track)
+            percussion_track: midi.Track = midi.Track()
+            for event in track:
+                event_copy: midi.Event = deepcopy(event)
+                if type(event_copy.event) == midi.NoteOnEvent and event_copy.event.data[0] != instrument.number:
+                        print("setting data = 0")
+                        event_copy.event.data[1] = 0
+
+                percussion_track.append(event_copy.event)
+            pattern.append(percussion_track)
+            print(f"Extracting {percussion_path}/{self.songname} - {self.track_number} - 0 - Drum Kit 0 - {instrument.name}.mid")
+            midi.write_midifile(f"{percussion_path}/{self.songname} - {self.track_number} - 0 - Drum Kit 0 - {instrument.name}.mid", pattern)
+
+    @staticmethod
+    def get_percussion_instruments(track: midi.Track) -> List[Percussion_Instrument]:
+        """
+            Get the list of percussion instruments from the MIDI track.
+
+            Args:
+                track: MIDI track.
+
+            Returns:
+                List[Percussion_Instrument]: List of percussion instruments.
+        """
+        percussion_instruments = set()
+        for event in track:
+            print(type(event.event))
+            if type(event.event) == midi.NoteOnEvent:
+                print(event.event.data[0])
+                percussion_instruments.add(Percussion_Instrument(event.event.data[0]))
+        percussion_instruments = list(percussion_instruments)
+        return percussion_instruments
