cc86ffe1b51f6440032558ba4588bddf972e8e66
Alex Gear <alexgear@gmail.com>
2023-11-25 21:36:44 -0500

    add doc string

--- percussion_instrument.py
+++ percussion_instrument.py
@@ -1,6 +1,20 @@
 from programs import PERCUSSION
 
 class Percussion_Instrument:
+    """
+    The Percussion_Instrument class represents a percussion instrument with a number and a name. It provides methods for comparing instruments, converting to a string, and generating a hash value.
+    Example Usage
+    # Create a percussion instrument with number 35
+    instrument = Percussion_Instrument(35)
+
+    # Print the name of the instrument
+    print(instrument)  # Output: "35 - Acoustic Bass Drum"
+
+    # Compare two instruments
+    instrument1 = Percussion_Instrument(35)
+    instrument2 = Percussion_Instrument(36)
+    print(instrument1 == instrument2)  # Output: False
+    """
     def __init__(self, number: int):
         if not (26 < number < 82):
             raise ValueError(f"Percussion Instrument number must be between 27 and 81. Received: {number}")
